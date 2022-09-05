3cc19b915d5a3d157129ea43227859bdac76f940
alexandergm <sasha_261@outlook.com>
2022-09-06 00:37:42 +0300

    Complete Task_41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,2 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите количество чисел, которые хотите ввести: ");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Количество положительных чисел = " + Sum());
+
+int Sum()
+{
+    int sum = 0;
+    for(int i = 1; i <= M ; i++)
+    {
+        Console.WriteLine($"Введите {i}-е число: ");
+        if (int.Parse(Console.ReadLine()) >= 0) sum++;        
+    }
+    return sum;
+}
