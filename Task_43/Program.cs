5ba36706731db0b49cab5dfe08c090de628b81fc
alexandergm <sasha_261@outlook.com>
2022-09-06 01:07:06 +0300

    Complete Task_43

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double x, y;
+Console.WriteLine("Введите значения значения b1, k1, b2 и k2 через пробел: ");
+string[] a = Console.ReadLine().Split(new Char[] { ' ' });
+double b1 = double.Parse(a[0]);
+double k1 = double.Parse(a[1]);
+double b2 = double.Parse(a[2]);
+double k2 = double.Parse(a[3]);
+Console.WriteLine(Intersection());
+
+string Intersection()
+{
+    string result = string.Empty;
+    if (k1 == k2 && b1 == b2)
+    {
+        result = "Прямые совпадают";
+    }
+    else if(k1 == k2)
+    {
+        result = "Прямые параллельны";
+    }
+    else
+    {
+        x = (b1-b2)/(k2-k1);
+        y = (k2 * b1 - k1 * b2) / (k2 - k1);
+        result = $"точка пересечения : x = {x}, y = {y}";
+    }    
+    return result;
+}
